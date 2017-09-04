413ac8d34a165898e4146f96443e0c9f5f8cb9b9
Changwei Liang <changwei777@gmail.com>
2017-09-04 15:07:49 -0700

    I have a piece of information to show

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
+from django.http import HttpResponse
 
-# Create your views here.
+
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
