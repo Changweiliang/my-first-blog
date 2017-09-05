286c9ea6bf6056f5894431309e1d537a270d309d
Changwei Liang <changwei777@gmail.com>
2017-09-04 23:30:18 -0700

    aiai

--- blog/tests.py
+++ blog/tests.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 from . import models
 
 # test git hub
+# test my personal laptop
 class PostModelTests(TestCase):
     def test_query_of_post_foreign_key(self):
         pass
