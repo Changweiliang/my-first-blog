608de6a26bce0e1b5c3c80f010b56290342dc840
Changwei Liang <changwei777@gmail.com>
2017-09-04 22:38:40 -0700

    new1

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url, include
 from . import views
 
+app_name = 'blog'
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
+    url(r'^(?P<pk>[0-9]+)/detail/$', views.PostDetail.as_view(), name='post_detail')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render,get_object_or_404
 from django.utils import timezone
 from .models import Post
-
-
+from django.views.generic import ListView, DetailView
 
 def post_list(request):
     post_blog_list = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
@@ -10,3 +9,7 @@ def post_list(request):
         'post_blog_list': post_blog_list,
     }
     return render(request, 'blog/post_list.html',context)
+
+class PostDetail(DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
