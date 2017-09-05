d6734fb86cc0088923e4e3f0997e0f8ff44ae070
Changwei Liang <changwei777@gmail.com>
2017-09-04 23:31:56 -0700

    aiaiai

--- blog/tests.py
+++ blog/tests.py
@@ -3,6 +3,7 @@ from . import models
 
 # test git hub
 # test my personal laptop
+# finally, i know how to work on two different computers with github
 class PostModelTests(TestCase):
     def test_query_of_post_foreign_key(self):
         pass
